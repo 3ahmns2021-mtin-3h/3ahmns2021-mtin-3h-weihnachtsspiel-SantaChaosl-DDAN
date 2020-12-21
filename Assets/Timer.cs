601d893f8b30267ed7653f86c4d21540f52bd716
Don_Denki <dantlerdavid@gmail.com>
2020-12-21 13:57:45 +0100

    Playercontrol and Timer included

---
+++ Assets/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+
+    public TextMeshProUGUI timer;
+
+    private float timeInSec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeInSec = 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeInSec -= Time.deltaTime;
+
+        float minutes = Mathf.FloorToInt(timeInSec/60);
+        float seconds = Mathf.FloorToInt(timeInSec%60);
+
+        if (timeInSec > 0)
+        {
+            Debug.Log("Tu doch etwas formatiert " + string.Format("{0:00}:{1:00}", minutes, seconds));
+            timer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+        else
+        {
+            Debug.Log("Stop Game");
+        }
+    }
+}
